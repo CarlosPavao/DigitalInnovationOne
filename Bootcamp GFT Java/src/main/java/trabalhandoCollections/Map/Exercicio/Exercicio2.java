4b4661cbde2834037b81b97558812c4ec4c2af23
Carlos Pavão <carlos.henrique93@msn.com>
2021-06-24 09:59:12 -0300

    Conclusão MAP

---
+++ Bootcamp GFT Java/src/main/java/trabalhandoCollections/Map/Exercicio/Exercicio2.java
@@ -0,0 +1,7 @@
+package trabalhandoCollections.Map.Exercicio;
+/*
+Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene.
+Depois, mostre quantas vezes cada valor foi conseguido.
+ */
+public class Exercicio2 {
+}
